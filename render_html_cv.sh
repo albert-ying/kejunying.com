3ad06423d2ae74f0564bf796494116c80591b1a9
albert-ying <59846322+albert-ying@users.noreply.github.com>
2025-07-23 21:23:06 +0800

    update prompt

--- render_html_cv.sh
+++ render_html_cv.sh
@@ -1,4 +1,4 @@
-#! bin/sh
+#!/bin/sh
 #
 cd content/cv
 docker run -ti --rm --mount src="$(pwd)",target=/pdf,type=bind pdf2htmlex/pdf2htmlex:0.18.8.rc2-master-20200820-alpine-3.12.0-x86_64 --data-dir pdf2htmlEX --zoom 1.2 Ying_cv.pdf
